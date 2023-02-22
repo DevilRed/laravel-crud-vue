8dcfaab27349fb35dd6015ef5b591bc6cccd4b64
Christian <chrisdaq@gmail.com>
2023-02-22 16:29:08 -0400

    add api resource for company model

--- app/Http/Resources/CompanyResource.php
+++ app/Http/Resources/CompanyResource.php
@@ -1,5 +1,10 @@
 <?php
+/*
+API resources to transform data
+https://laravel.com/docs/master/eloquent-resources
 
+php artisan make:resource CompanyResource
+ */
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
