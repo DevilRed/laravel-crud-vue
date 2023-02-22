f49ed51dc8a9cb7322d116a0f225ccdc07442eee
Christian <chrisdaq@gmail.com>
2023-02-22 16:40:16 -0400

    add vue routes

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -3,7 +3,9 @@ import { createRouter, createWebHistory } from "vue-router";
 import CompaniesIndex from '@/components/companies/CompaniesIndex.vue'
 import CompaniesCreate from '@/components/companies/CompaniesCreate.vue'
 import CompaniesEdit from '@/components/companies/CompaniesEdit.vue'
-
+/*
+add routes on vue
+ */
 const routes = [
     {
         path: '/dashboard',
